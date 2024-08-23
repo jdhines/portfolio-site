1097526fa99528dad006c3fbac2d4034196a1c84
Hines, Jess <Jess.Hines@fmr.com>
2024-08-22 18:55:31 -0700

    Finish Mediq page

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -4,13 +4,14 @@ export function Nav() {
       <a class="title bg-white" href="/">
         <img width="40" src="/logo.svg" title="JH logo: a work in progress, typographically and personally" alt=""/>
       </a>
-      <div class = "dropdown">
+      <a class = "item" href="/portfolio">Portfolio</a>
+      {/* <div class = "dropdown">
         <a class = "item" href="/portfolio">Portfolio</a>
         <div class = "items">
           <a class = "item">UX Design</a>
           <a class = "item">Graphic Design</a>
         </div>
-      </div>
+      </div> */}
       <a class="item" href="/experience">
         Experience
       </a>
