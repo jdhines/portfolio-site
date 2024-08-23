8cc113474181cd8f10b13f3b528bf16ab345ba72
Hines, Jess <jessdhines@gmail.com>
2024-08-23 16:27:57 -0700

    Fix typo in config

--- dev.ts
+++ dev.ts
@@ -1,5 +1,5 @@
 #!/usr/bin/env -S deno run -A --watch=static/,routes/
 import dev from "$fresh/dev.ts";
-import config from "./fresh.config";
+import config from "./fresh.config.ts";
 
 await dev(import.meta.url, "./main.ts", config);
